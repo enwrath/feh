30aa5a8e967a96e97d119e0b12d6ac0afeeca649
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-08-10 17:07:06 +0300

    Add simple spinner for loading data

---
+++ src/components/Loading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="visible" class="loader">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    visible: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.loader {
+  margin: auto;
+  border: 0.75em solid #5f5d5d;
+  border-right: 0.75em solid #68f784;
+  border-radius: 50%;
+  width: 3em;
+  height: 3em;
+  animation: spin 1.5s ease-in-out infinite;
+}
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(720deg); }
+}
+</style>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Upcoming legendary/mythic banners</h1>
+    <Loading v-bind:visible="loadingHeroes || loadingBanners" />
     <div v-bind:key="banner.title" v-for="banner in banners">
       <Banner v-bind:banner="banner" v-bind:heroes="heroes" />
     </div>
@@ -11,26 +12,29 @@
 
 // @ is an alias to /src
 import Banner from '@/components/Banner.vue'
+import Loading from '@/components/Loading.vue';
 
 export default {
   name: 'Calendar',
   components: {
-    Banner
+    Loading, Banner
   },
   data() {
     return {
       heroes: {},
-      banners: []
+      banners: [],
+      loadingHeroes: true,
+      loadingBanners: true
     }
   },
   async created() {
     fetch("https://api.jsonbin.io/b/5f313b621823333f8f20ef64")
       .then(res => res.json())
-      .then(data => this.heroes = data);
+      .then(data => {this.heroes = data; this.loadingHeroes = false;});
 
     fetch("https://api.jsonbin.io/b/5f313b4b1823333f8f20ef5a")
       .then(res => res.json())
-      .then(data => this.banners = data);
+      .then(data => {this.banners = data; this.loadingBanners = false;});
   }
 }
 </script>
