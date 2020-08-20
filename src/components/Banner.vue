e0bd77b5fa2e33fb0af8f2722a90124729422fa5
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-08-20 18:25:12 +0300

    Changed to 4x3 listing instead of 3x4

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,8 +2,9 @@
   <div>
     <h2>{{banner.title}}</h2>
     <div class="bannerHeroes">
-      <BannerSlot v-bind:key="`${i}-${hero}`" v-for="(hero, i) in banner.heroes" v-bind:hero="heroes[hero]" />
-
+      <BannerSlot v-bind:key="`R1${i}-${hero}`" v-for="(hero, i) in banner.heroes.filter((x,i) => [0,3,6,9].includes(i))" v-bind:hero="heroes[hero]" />
+      <BannerSlot v-bind:key="`R2${i}-${hero}`" v-for="(hero, i) in banner.heroes.filter((x,i) => [1,4,7,10].includes(i))" v-bind:hero="heroes[hero]" />
+      <BannerSlot v-bind:key="`R3${i}-${hero}`" v-for="(hero, i) in banner.heroes.filter((x,i) => [2,5,8,11].includes(i))" v-bind:hero="heroes[hero]" />
     </div>
   </div>
 </template>
@@ -29,24 +30,12 @@ export default {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: center;
-  max-width: 1500px;
   margin:auto;
+  max-width: 640px;
+
 }
 .bannerHeroes > div {
-  width: 158px;
-  flex-basis: 15%;
-}
-@media only screen and (max-width: 1000px) {
-  .bannerHeroes > div {
-    flex-basis: 30%;
-    font-size: small;
-  }
-}
-@media only screen and (max-width: 500px) {
-  .bannerHeroes > div {
-    flex-basis: 30%;
-    font-size: x-small;
-  }
+  width: 25%;
+  height: 33%;
 }
 </style>
