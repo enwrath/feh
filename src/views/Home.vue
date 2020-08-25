8f1d62a19aef07a8047c0ab9fde56d98fde84869
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-08-25 16:21:43 +0300

    Remove useless Home page and move calendar to top

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="home">
-    <p>
-      This is a fan site for Fire Emblem: Heroes. Work-in-progress.
-    </p>
+  <div>
+    <h1>Upcoming legendary/mythic banners</h1>
+    <div class="bannerList">
+      <div v-bind:key="banner.title" v-for="banner in banners">
+        <Banner v-bind:banner="banner" v-bind:heroes="heroes" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 // @ is an alias to /src
+import Banner from '@/components/Banner.vue'
+import bannerjson from "@/assets/banners.json";
+import herojson from "@/assets/heroes.json";
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Banner
+  },
+  data() {
+    return {
+      heroes: herojson,
+      banners: bannerjson
+    }
+  }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bannerList {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.bannerList > div {
+  margin: auto;
+  margin-bottom: 1em;
+  background-color: #b5b5b5;
+}
+</style>
